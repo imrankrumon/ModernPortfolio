df8c6ede1117148b32a1d0e3251539a1971cb7b4
Imran Rumon <post2imran0701@gmail.com>
2021-10-26 11:28:29 +0600

    Menu button & rotating

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,11 @@
 'use strict'
+
+const menuBtn = document.querySelector('.header__menu--btn');
+const menuNav = document.querySelector('.header__menu--nav');
+const menuBranding = document.querySelector('.header__menu--branding');
+const menuItemsContainer = document.querySelector('.header__menu--items');
+const menuItems = document.querySelectorAll('.header__menu--item');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('open');
+})
